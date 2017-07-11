f2b84e1a2d621a0e8f56c46e16b3cffa74450874
Kallebe Felipe <kallebefelipe@gmail.com>
2017-07-11 16:24:00 -0300

    Paginacao

--- quotes.py
+++ quotes.py
@@ -8,8 +8,21 @@ class QuotesSpider(scrapy.Spider):
     start_urls = ['http://jconline.ne10.uol.com.br/canal/cidades/policia/']
 
     def parse(self, response):
-        for quote in response.css('ul.box-lista-noticias'):
-            yield {
-                # 'titulo' : response.css('h1.bloco_titulo.esq::text').extract_first(),
-                'noticia' : response.css('a::text').extract()
-            }
+        urls = response.css('li.lista-noticia-item > a::attr(href)').extract()
+        for url in urls:
+            url = response.urljoin(url)
+            yield scrapy.Request(url=url, callback=self.detalhes_noticia)
+
+        next_page_url = response.css('li.next > a::attr(href)').extract_first()
+        if next_page_url:
+            next_page_url = response.urljoin(next_page_url)
+            yield scrapy.Request(url=next_page_url, callback=self.parse)
+
+    def detalhes_noticia(self, response):
+        print(response.text)
+        yield {
+            'titulo': response.css('h1.titulo-materia::text').extract_first(),
+            # 'bairro': response.css('div.mg_chapeu::text').extract_first(),
+            # 'texto': response.css('div.noticia_corponoticia > p::text').extract_first(),
+            # 'tag': response.css('div.mg_chapeu::text').extract_first(),
+        }
