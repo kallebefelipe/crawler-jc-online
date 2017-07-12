d0d3deb5a48cbb77b040f02c71774b86b00179de
Kallebe Felipe <kallebefelipe@gmail.com>
2017-07-12 04:03:18 -0300

    Adicionando paginacao com selenium

--- quotes.py
+++ quotes.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
 
-
 class QuotesSpider(scrapy.Spider):
     name = 'quotes'
     allowed_domains = ['jconline.ne10.uol.com.br/canal/cidades/policia/']
@@ -11,7 +10,7 @@ class QuotesSpider(scrapy.Spider):
         urls = response.css('li.lista-noticia-item > a::attr(href)').extract()
         for url in urls:
             url = response.urljoin(url)
-            yield scrapy.Request(url=url, callback=self.detalhes_noticia)
+            yield scrapy.Request(url=url, callback=self.detalhes_noticia, dont_filter=True)
 
         next_page_url = response.css('li.next > a::attr(href)').extract_first()
         if next_page_url:
@@ -19,10 +18,23 @@ class QuotesSpider(scrapy.Spider):
             yield scrapy.Request(url=next_page_url, callback=self.parse)
 
     def detalhes_noticia(self, response):
-        print(response.text)
         yield {
             'titulo': response.css('h1.titulo-materia::text').extract_first(),
             # 'bairro': response.css('div.mg_chapeu::text').extract_first(),
             # 'texto': response.css('div.noticia_corponoticia > p::text').extract_first(),
             # 'tag': response.css('div.mg_chapeu::text').extract_first(),
         }
+
+# Snippet imported from snippets.scrapy.org (which no longer works)
+
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome(executable_path='/home/kallebe/Downloads/chromedriver')
+driver.get("http://jconline.ne10.uol.com.br/canal/cidades/policia/")
+while True:
+    # driver.find_element_by_xpath(".//li[@class='paginacao-item']/[@rel='20']").click()
+    # driver.find_element_by_xpath("//[@rel='20']").click()
+    driver.find_element_by_link_text('Próxima').click()
+    time.sleep(10)
